ff54f59d79fd459de045d167eb3f4bae07f6666f
Vikash Tiwari <tiwari.vikash@gmail.com>
2024-06-30 18:32:52 -0400

    finished 03

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,63 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Order {
+        if product_name == "" {
+            panic!("product_name can not be empty")
+        }
+
+        if product_name.len() > 300 {
+            panic!("product_name can not be longer than 300 bytes")
+        }
+
+        if quantity <= 0 {
+            panic!("quantity can not be zero")
+        }
+
+        if unit_price <= 0 {
+            panic!("unit_price can not be zero")
+        }
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        self.product_name = new_product_name;
+    }
+
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: i32) {
+        self.quantity = new_quantity;
+    }
+
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: i32) {
+        self.unit_price = new_unit_price;
+    }
+
+    pub fn total(&self) -> i32 {
+        self.quantity * self.unit_price
+    }
+}
